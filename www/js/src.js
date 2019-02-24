6fb1bdd20faaea7fd3a6c46d06b92398a64862e1
N__Bear <meduator33@gmail.com>
2019-02-24 22:51:07 +0200

    start footer

--- www/js/src.js
+++ www/js/src.js
@@ -1,12 +1,27 @@
-var gridItemImg = document.querySelector('.grid-item img');
-// hiden-info == grid-item img 
+// This does the filter by binding an event on the change of a select box
+$("#grid-filter li").click(function() {
+  var group = $(this).data('category');
+  var group_class = "." + group;
+  
+  if(group == "*"){
+    $(".grid-item").show();
+    $('#masonry-grid').masonry('layout');
+  }
+  else if(group != "") {
+    $(".grid-item").hide();
+    $(group_class).show();
+    $('#masonry-grid').masonry('layout');
+  } else {
+    $(".grid-item").show();
+    $('#masonry-grid').masonry('layout');
+  }
+});
+
 
 $('#masonry-grid').masonry({
   columnWidth: 1,
   itemSelector: '.grid-item'
 });
-
-
 
 $(".toggle-main-nav").click(function() {
   $(".sandwich").toggleClass("active");
